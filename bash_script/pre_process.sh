a5dbdcf4b416f5ec456a70f543b058aaffa1ac0f
Lingcao Huang <huanglingcao@gmail.com>
2017-10-21 21:08:48 +0800

    bug fix

--- bash_script/pre_process.sh
+++ bash_script/pre_process.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 para_file=para.ini
+para_py=/home/hlc/codes/PycharmProjects/DeeplabforRS/parameters.py
 
-root=$(python2 parameters.py -p ${para_file} working_root)
-train_shp=$(python2 parameters.py -p ${para_file} training_polygons)
-eo_dir=$(python2 parameters.py -p ${para_file} codes_dir)
+root=$(python2 ${para_py} -p ${para_file} working_root)
+train_shp=$(python2 ${para_py} -p ${para_file} training_polygons)
+eo_dir=$(python2 ${para_py} -p ${para_file} codes_dir)
 
 
-input_image=$(python2 parameters.py -p ${para_file} input_image_path)
+input_image=$(python2 ${para_py} -p ${para_file} input_image_path)
 # input groud truth (raster data with pixel value)
-input_GT=$(python2 parameters.py -p ${para_file} input_ground_truth_image)
+input_GT=$(python2 ${para_py} -p ${para_file} input_ground_truth_image)
 
 # current folder (without path)
 test_dir=${PWD##*/}
@@ -22,16 +23,16 @@ rm ${root}/${test_dir}/split_labels/*
 mkdir ${root}/${test_dir}/top ${root}/${test_dir}/gts_numpy ${root}/${test_dir}/split_images ${root}/${test_dir}/split_labels
 
 ###pre-process UAV images, extract the training data from the whole image
-dstnodata=$(python2 parameters.py -p ${para_file} dst_nodata)
-buffersize=$(python2 parameters.py -p ${para_file} buffer_size)
-rectangle_ext=$(python2 parameters.py -p ${para_file} b_use_rectangle)
+dstnodata=$(python2 ${para_py} -p ${para_file} dst_nodata)
+buffersize=$(python2 ${para_py} -p ${para_file} buffer_size)
+rectangle_ext=$(python2 ${para_py} -p ${para_file} b_use_rectangle)
 
 ${eo_dir}/extract_target_imgs.py -n ${dstnodata} -b ${buffersize} ${rectangle_ext} $train_shp ${input_image}  -o ${root}/${test_dir}/top
 ${eo_dir}/extract_target_imgs.py -n ${dstnodata} -b ${buffersize} ${rectangle_ext} $train_shp ${input_GT}  -o ${root}/${test_dir}/gts_numpy
 
 ### split the training image to many small patch
-patch_w=$(python2 parameters.py -p ${para_file} patch_width)
-patch_h=$(python2 parameters.py -p ${para_file} patch_height)
+patch_w=$(python2 ${para_py} -p ${para_file} patch_width)
+patch_h=$(python2 ${para_py} -p ${para_file} patch_height)
 
 for img in $(ls ${root}/${test_dir}/top/*.tif)
 do
