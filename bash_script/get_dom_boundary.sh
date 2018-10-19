47adcb531c1d2c5de71e3582a646254abece788e
Lingcao Huang <huanglingcao@gmail.com>
2018-10-18 19:32:17 -0600

    bug fix

--- bash_script/get_dom_boundary.sh
+++ bash_script/get_dom_boundary.sh
@@ -4,21 +4,23 @@
 
 DOM=~/Data/eboling/eboling_uav_images/dom/UAV_DOM_Eboling.tif
 DOM_tmp=~/Data/eboling/eboling_uav_images/dom/UAV_DOM_Eboling_tmp.tif
-DOM_tmp2=~/Data/eboling/eboling_uav_images/dom/UAV_DOM_Eboling_tmp2.tif
+#DOM_tmp2=~/Data/eboling/eboling_uav_images/dom/UAV_DOM_Eboling_tmp2.tif
 
 mkdir ~/Data/eboling/eboling_uav_images/dom/dom_extent
 out_shp=~/Data/eboling/eboling_uav_images/dom/dom_extent/dom_extent.shp
 
-# add no-data
-gdalwarp -dstnodata 0 -of GTiff ${DOM} ${DOM_tmp}
+## add no-data
+#gdalwarp -dstnodata 0 -of GTiff ${DOM} ${DOM_tmp}
+#
+## add band 4 as mask band
+#gdalwarp -dstalpha -of GTiff ${DOM_tmp} ${DOM_tmp2}
 
-# add band 4 as mask band
-gdalwarp -dstalpha -of GTiff ${DOM_tmp} ${DOM_tmp2}
+# the nodata value is zero
+gdal_calc.py -A ${DOM} --A_band=1 --outfile=${DOM_tmp} --calc="255*(A>0)" --NoDataValue=0
 
 # get the boundary based on band 4
-gdal_polygonize.py ${DOM_tmp2} -b 4 -f "ESRI Shapefile" ${out_shp}
-
+# the result turns out have three polygons (two more tiny polygons, so I delete them manually)
+gdal_polygonize.py ${DOM_tmp} -b 1 -f "ESRI Shapefile" ${out_shp}
 
 #
 rm ${DOM_tmp}
-rm ${DOM_tmp2}
