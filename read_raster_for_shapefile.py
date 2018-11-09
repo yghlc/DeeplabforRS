913d776120d4a92caba5834b0c4617a7916e9400
cryoyan <yan_hu@hotmail.com>
2018-11-09 21:40:45 +0800

    read length

--- read_raster_for_shapefile.py
+++ read_raster_for_shapefile.py
@@ -130,13 +130,15 @@ def main(options, args):
         start_value = read_dem_basedON_location(start_point[idx][0], start_point[idx][1], raster)
         # read value of end point
         end_value = read_dem_basedON_location(end_point[idx][0], end_point[idx][1], raster)
+        # read value of length
+        length_value = length[idx]
 
         print(start_value, end_value)
-
+        print(length_value)
         # calculate
 
-        slope_angle = math.atan((start_value-end_value) / length)
-        print(slope_angle)
+       # slope_angle = math.atan((start_value-end_value) / length)
+       # print(slope_angle)
 
 
     pass
