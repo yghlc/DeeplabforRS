bee70a328904c62d438c18d5810de807bae61c9c
yghlc <hlchuang@126.com>
2019-01-15 11:22:50 -0700

    new version of pyshp

--- basic_src/RSImageProcess.py
+++ basic_src/RSImageProcess.py
@@ -11,7 +11,7 @@ add time: 05 May, 2016
 # support for python3: import the script in the same dir. hlc 2019-Jan-15
 import sys,os
 py_dir=os.path.dirname(os.path.realpath(__file__))
-print(py_dir)
+# print(py_dir)
 sys.path.append(py_dir)
 
 from RSImage import RSImageclass
