d5218975b3f6940f29de1e0918450fc32b0951e0
Lingcao Huang <huanglingcao@gmail.com>
2020-01-31 15:27:34 +0800

    user pyshp > 2.0

--- vector_features.py
+++ vector_features.py
@@ -19,8 +19,9 @@ import  parameters
 
 #pyshp library
 import shapefile
-if shapefile.__version__ >= '2.0.0':
-    raise ValueError('Current do not support pyshp version 2 or above, please use pyshp version 1.2.12')
+# code code need to consider the new version of pyshp 31 Jan 2020.
+# if shapefile.__version__ >= '2.0.0':
+#     raise ValueError('Current do not support pyshp version 2 or above, please use pyshp version 1.2.12')
 
 import random
 
