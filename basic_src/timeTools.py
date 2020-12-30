a4ae78d5d06bf4d318430af42d2e49ad71bc03ce
Lingcao Huang <huanglingcao@gmail.com>
2020-12-29 19:14:04 -0700

    add

---
+++ basic_src/timeTools.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# Filename: timeTools 
+"""
+introduction: functions and classes to handle datetime
+
+authors: Huang Lingcao
+email:huanglingcao@gmail.com
+add time: 29 December, 2020
+"""
+
+import os,sys
+import basic_src.basic as basic
+
+from datetime import datetime
+from dateutil.parser import parse
+import re
+
+def get_yeardate_yyyymmdd(in_string):
+    '''
+    get datetime object from a filename (or string)
+    Args:
+        in_string: input string containing yyyymmdd
+
+    Returns: datetime
+
+    '''
+    pattern = '[0-9]{8}'
+    format = '%Y%m%d'
+    found_strs = re.findall(pattern,in_string)
+    if len(found_strs) < 1:
+        basic.outputlogMessage('Warning, cannot found yyyymmdd string in %s'%(in_string))
+        return None
+    # for str in found_strs:
+    #     print(str)
+    #     date_obj = parse(str,yearfirst=True)
+    #     print(date_obj)
+
+    datetime_list = []
+    for str in found_strs:
+        try:
+            date_obj = datetime.strptime(str, format)
+            # print(date_obj)
+            datetime_list.append(date_obj)
+        except ValueError as e:
+            # print(e)
+            pass
+
+    # print(datetime_list)
+    if len(datetime_list) != 1:
+        basic.outputlogMessage('Warning, found %d yyyymmdd string in %s' % (len(datetime_list), in_string))
+        return None
+    return datetime_list[0]
+
+
+
+def test():
+    out = get_yeardate_yyyymmdd('20170301_10300100655B5A00_1030010066B4AA00.tif')
+    print(out)
+
+
+if __name__=='__main__':
+    test()
+    pass
