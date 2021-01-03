27cb502af6327c679fc822a5e7840707695cfacf
Lingcao Huang <huanglingcao@gmail.com>
2021-01-03 14:42:42 -0700

    add

---
+++ raster_io.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Filename: raster_io 
+"""
+introduction:  Based on rasterio, to read and write raster data
+
+authors: Huang Lingcao
+email:huanglingcao@gmail.com
+add time: 03 January, 2021
+"""
+
+import os, sys
+from optparse import OptionParser
+import rasterio
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__=='__main__':
+    main()
