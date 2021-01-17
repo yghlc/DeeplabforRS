3332faea964b13decb29d790d101ab1b642f64df
Lingcao Huang <huanglingcao@gmail.com>
2021-01-17 13:28:44 -0700

    not calculate shapeinfo

--- polygon_post_process.py
+++ polygon_post_process.py
@@ -319,16 +319,16 @@ def main(options, args):
         basic.outputlogMessage("warning, mapped_polygon_narrow_threshold is not in the parameter file, skip removing narrow parts")
 
     # calculate area, perimeter of polygons
+    if cal_add_area_length_of_polygon(output) is False:
+        return False
+
+    # calculate the polygon information
     b_calculate_shape_info = parameters.get_bool_parameters_None_if_absence('','b_calculate_shape_info')
     if b_calculate_shape_info:
         # remove "_shapeInfo.shp" to make it calculate shape information again
         os.system('rm *_shapeInfo.shp')
-        if cal_add_area_length_of_polygon(output) is False:
+        if calculate_gully_information(output) is False:
             return False
-
-    # calculate the polygon information
-    if calculate_gully_information(output) is False:
-        return False
 
     # # remove small and not narrow polygons
     # if options.min_area is None:
