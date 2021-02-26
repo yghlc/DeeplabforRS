a3fe63a61492fedf22dbb27c86fa205f1ec4d07a
Lingcao Huang <hlchuang@126.com>
2021-02-26 06:17:39 -0700

    update to the new version

--- split_image.py
+++ split_image.py
@@ -10,10 +10,6 @@ add time: 15 July, 2017
 import sys,os,subprocess
 from optparse import OptionParser
 
-# def sliding_window(image_width,image_height, patch_w,patch_h,adj_overlay=0):
-#     sliding_window(image_width, image_height, patch_w, patch_h, adj_overlay_x=adj_overlay, adj_overlay_y=adj_overlay)
-
-
 def sliding_window(image_width,image_height, patch_w,patch_h,adj_overlay_x=0,adj_overlay_y=0):
     """
     get the subset windows of each patch
@@ -45,18 +41,18 @@ def sliding_window(image_width,image_height, patch_w,patch_h,adj_overlay_x=0,adj
         count_y = count_y + 1
 
     # output split information
-    f_obj = open('split_image_info.txt','w')
-    f_obj.writelines('### This file is created by split_image.py. mosaic_patches.py need it. Do not edit it\n')
-    f_obj.writelines('image_width:%d\n' % image_width)
-    f_obj.writelines('image_height:%d\n' % image_height)
-    f_obj.writelines('expected patch_w:%d\n' % patch_w)
-    f_obj.writelines('expected patch_h:%d\n'%patch_h)
-    f_obj.writelines('adj_overlay_x:%d\n' % adj_overlay_x)
-    f_obj.writelines('adj_overlay_y:%d\n' % adj_overlay_y)
+    # f_obj = open('split_image_info.txt','w')
+    # f_obj.writelines('### This file is created by split_image.py. mosaic_patches.py need it. Do not edit it\n')
+    # f_obj.writelines('image_width:%d\n' % image_width)
+    # f_obj.writelines('image_height:%d\n' % image_height)
+    # f_obj.writelines('expected patch_w:%d\n' % patch_w)
+    # f_obj.writelines('expected patch_h:%d\n'%patch_h)
+    # f_obj.writelines('adj_overlay_x:%d\n' % adj_overlay_x)
+    # f_obj.writelines('adj_overlay_y:%d\n' % adj_overlay_y)
 
     patch_boundary = []
     for i in range(0,count_x):
-        f_obj.write('column %d:'%i)
+        # f_obj.write('column %d:'%i)
         for j in range(0,count_y):
             w = patch_w
             h = patch_h
@@ -65,7 +61,7 @@ def sliding_window(image_width,image_height, patch_w,patch_h,adj_overlay_x=0,adj
             if j == count_y - 1:
                 h = leftH
 
-            f_obj.write('%d ' % (i*count_y + j))
+            # f_obj.write('%d ' % (i*count_y + j))
 
             # extend the patch
             xoff = max(i*patch_w - adj_overlay_x,0)  # i*patch_w
@@ -76,16 +72,14 @@ def sliding_window(image_width,image_height, patch_w,patch_h,adj_overlay_x=0,adj
             new_patch = (xoff,yoff ,xsize, ysize)
             patch_boundary.append(new_patch)
 
-        f_obj.write('\n')
+        # f_obj.write('\n')
 
-    f_obj.close()
+    # f_obj.close()
     return patch_boundary
 
 
-# def split_image(input,output_dir,patch_w=1024,patch_h=1024,adj_overlay=0):
-#     split_image(input, output_dir, patch_w=1024, patch_h=1024, adj_overlay_x=adj_overlay, adj_overlay_y=adj_overlay)
 
-def split_image(input,output_dir,patch_w=1024,patch_h=1024,adj_overlay_x=0,adj_overlay_y=0):
+def split_image(input,output_dir,patch_w=1024,patch_h=1024,adj_overlay_x=0,adj_overlay_y=0,out_format='PNG', pre_name = None):
     """
     split a large image to many separate patches
     Args:
@@ -98,11 +92,9 @@ def split_image(input,output_dir,patch_w=1024,patch_h=1024,adj_overlay_x=0,adj_o
 
     """
     if os.path.isfile(input) is False:
-        print("Error: %s file not exist"%input)
-        return False
+        raise IOError("Error: %s file not exist"%input)
     if os.path.isdir(output_dir) is False:
-        print("Error: %s Folder not exist" % input)
-        return False
+        raise IOError("Error: %s Folder not exist" % input)
 
     Size_str = os.popen('gdalinfo '+input + ' |grep Size').readlines()
     temp = Size_str[0].split()
@@ -110,25 +102,37 @@ def split_image(input,output_dir,patch_w=1024,patch_h=1024,adj_overlay_x=0,adj_o
     img_height = int(temp[3])
 
     print('input Width %d  Height %d'%(img_witdh,img_height))
-
+    # print(('patch Width %d  Height %d'%(patch_w,patch_h)))
     patch_boundary = sliding_window(img_witdh,img_height,patch_w,patch_h,adj_overlay_x,adj_overlay_y)
 
+    # remove duplicated patches
+    patch_boundary_unique = set(patch_boundary)
+    if len(patch_boundary_unique) != len(patch_boundary):
+        print('remove %d duplicated patches of images %s'%(len(patch_boundary)-len(patch_boundary_unique),
+              os.path.basename(input)))
+        patch_boundary = patch_boundary_unique
+
     index = 0
-    pre_name = os.path.splitext(os.path.basename(input))[0]
-    f_obj = open('split_image_info.txt', 'a+')
-    f_obj.writelines("pre FileName:"+pre_name+'_p_\n')
-    f_obj.close()
+    if pre_name is None:
+        pre_name = os.path.splitext(os.path.basename(input))[0]
+    # f_obj = open('split_image_info.txt', 'a+')
+    # f_obj.writelines("pre FileName:"+pre_name+'_p_\n')
+    # f_obj.close()
 
     for patch in patch_boundary:
         # print information
         print(patch)
-        output_path = os.path.join(output_dir,pre_name+'_p_%d.tif'%index)
-        args_list = ['gdal_translate','-ot','Byte','-srcwin',str(patch[0]),str(patch[1]),str(patch[2]),str(patch[3]), input, output_path]
+        if out_format.upper()=='PNG':
+            output_path = os.path.join(output_dir,pre_name+'_p_%d.png'%index)
+        elif out_format.upper()=='GTIFF':   #GTiff
+            output_path = os.path.join(output_dir, pre_name + '_p_%d.tif' % index)
+        else:
+            raise ValueError("unknow output format:%s"%out_format)
+        args_list = ['gdal_translate','-of',out_format,'-srcwin',str(patch[0]),str(patch[1]),str(patch[2]),str(patch[3]), input, output_path]
         ps = subprocess.Popen(args_list)
         returncode = ps.wait()
         if os.path.isfile(output_path) is False:
-            print('Failed in gdal_translate, return codes: ' + str(returncode))
-            return False
+            raise IOError('Failed in gdal_translate, return codes: ' + str(returncode))
         index = index + 1
 
 
@@ -155,10 +159,12 @@ def main(options, args):
     if os.path.isdir(out_dir) is False:
         os.makedirs(out_dir)
 
+    out_format = options.out_format
+
     image_path = args[0]
 
-    # split_image(image_path,out_dir,patch_width,patch_height,adj_overlay)
-    split_image(image_path,out_dir,patch_width,patch_height,adj_overlay, adj_overlay)
+    split_image(image_path,out_dir,patch_width,patch_height,adj_overlay,adj_overlay,out_format)
+
 
     pass
 
@@ -178,6 +184,9 @@ if __name__ == "__main__":
     parser.add_option("-o", "--out_dir",
                       action="store", dest="out_dir",
                       help="the folder path for saving output files")
+    parser.add_option("-f", "--out_format",
+                      action="store", dest="out_format",default='PNG',
+                      help="the format of output images")
 
     (options, args) = parser.parse_args()
     if len(sys.argv) < 2 or len(args) < 1:
