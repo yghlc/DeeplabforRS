2ebc3f4af335f40576419282945e4e598a598946
Lingcao Huang <huanglingcao@gmail.com>
2021-09-07 20:39:50 -0600

    backup, not working yet

--- polygonize_large.py
+++ polygonize_large.py
@@ -22,6 +22,9 @@ import timeTools
 import vector_features
 from datetime import datetime
 
+import multiprocessing
+from multiprocessing import Pool
+
 import basic_src.io_function as io_function
 import basic_src.map_projection as map_projection
 
@@ -55,7 +58,15 @@ def read_polygons_from_small_patch(in_shp,in_raster):
     return polygon_no_touch,polygons_touch
 
 
-def raster2shapefile_large(in_raster, working_dir, out_shp=None,connect8=True):
+def polygonzie_read_one_patch(idx, tif, patch_count,connect8):
+    patch_shp = polygonzie_one_small_raster(idx, tif, patch_count, None, connect8=connect8)
+
+    # read polygons,
+    poly_no_touch, poly_touch = read_polygons_from_small_patch(patch_shp, tif)
+
+    return poly_no_touch, poly_touch
+
+def raster2shapefile_large(in_raster, working_dir, out_shp=None,connect8=True,process_num=1):
 
     if out_shp is None:
         out_shp = os.path.splitext(in_raster)[0] + '.shp'
@@ -67,6 +78,7 @@ def raster2shapefile_large(in_raster, working_dir, out_shp=None,connect8=True):
         io_function.mkdir(working_dir)
 
     pre_name = os.path.splitext(os.path.basename(in_raster))[0]
+    wkt = map_projection.get_raster_or_vector_srs_info_proj4(in_raster)
 
     # height, width, band_num, date_type = raster_io.get_height_width_bandnum_dtype(in_raster)
     # print('input image: height, width, band_num, date_type',height, width, band_num, date_type)
@@ -78,23 +90,43 @@ def raster2shapefile_large(in_raster, working_dir, out_shp=None,connect8=True):
     polygons_no_touch = []
     polygons_touch_edge = []
 
-    for idx, tif in enumerate(out_raster_list):
-        patch_shp = polygonzie_one_small_raster(idx, tif , patch_count, in_raster,connect8=connect8)
-        patch_shps.append(patch_shp)
+    if process_num == 1:
+        for idx, tif in enumerate(out_raster_list):
+            patch_shp = polygonzie_one_small_raster(idx, tif , patch_count, in_raster,connect8=connect8)
+            patch_shps.append(patch_shp)
 
-        # read polygons,
-        poly_no_touch, poly_touch = read_polygons_from_small_patch(patch_shp, tif)
-        polygons_no_touch.extend(poly_no_touch)
-        polygons_touch_edge.extend(poly_touch)
+            # read polygons,
+            poly_no_touch, poly_touch = read_polygons_from_small_patch(patch_shp, tif)
+            polygons_no_touch.extend(poly_no_touch)
+            polygons_touch_edge.extend(poly_touch)
+    elif process_num > 1:
+        theadPool = Pool(process_num)  # multi processes
+        parameters_list = [(idx,tif, patch_count,True) for idx,tif in enumerate(out_raster_list)]
+        results = theadPool.starmap(polygonzie_read_one_patch, parameters_list)  # need python3
+        for poly_no_touch, poly_touch in results:
+            polygons_no_touch.extend(poly_no_touch)
+            polygons_touch_edge.extend(poly_touch)
+        theadPool.close()
+    else:
+        raise ValueError('Wrong process number %s '%str(process_num))
+
+    # save touch edge to file (for test)
+    save_pd = pd.DataFrame({'Polygons':polygons_touch_edge})
+    save_polygon_edge = os.path.splitext(out_shp)[0] + '_polyTouchEdge.gpkg'
+    vector_gpd.save_polygons_to_files(save_pd,'Polygons',wkt,save_polygon_edge,format='GPKG')
+    save_polygon_edge_2 = os.path.splitext(out_shp)[0] + '_polyTouchEdge.shp'
+    vector_gpd.save_polygons_to_files(save_pd,'Polygons',wkt,save_polygon_edge_2)
+    sys.exit(0)
 
     print('count of polygons touch patch edge', len(polygons_touch_edge))
     # merge all polygons touch edge
     print(timeTools.get_now_time_str(), 'start building adjacent_matrix')
-    machine_name = os.uname()[1]
     # if 'login' in machine_name or 'shas' in machine_name or 'sgpu' in machine_name:
     #     print('Warning, some problem of parallel running in build_adjacent_map_of_polygons on curc, but ok in my laptop and uist, change process_num = 1')
     #     process_num = 1
-    adjacent_matrix = vector_gpd.build_adjacent_map_of_polygons(polygons_touch_edge, process_num=1)
+
+    polygons_touch_edge_buff = [item.buffer(0.1) for item in polygons_touch_edge]
+    adjacent_matrix = vector_gpd.build_adjacent_map_of_polygons(polygons_touch_edge_buff, process_num=process_num)
     print(timeTools.get_now_time_str(), 'finish building adjacent_matrix')
 
     if adjacent_matrix is False:
@@ -107,8 +139,7 @@ def raster2shapefile_large(in_raster, working_dir, out_shp=None,connect8=True):
     id_list = [idx for idx in range(len(polygons_no_touch)) ]
 
     save_pd = pd.DataFrame({'id':id_list,'Polygons':polygons_no_touch})
-    wkt = map_projection.get_raster_or_vector_srs_info_proj4(in_raster)
-    vector_gpd.save_polygons_to_files(save_pd,'Polygon',wkt,out_shp)
+    vector_gpd.save_polygons_to_files(save_pd,'Polygons',wkt,out_shp)
 
 
 
@@ -117,7 +148,8 @@ def test_raster2shapefile_large():
     working_dir = os.path.expanduser('~/Data/dem_processing/grid_9053_tmp_files/20140701_dem_slope_bin_patches')
 
     out_shp = os.path.expanduser('~/Data/dem_processing/grid_9053_tmp_files/20140701_dem_slope_bin_patches_all.shp')
-    raster2shapefile_large(in_tif, working_dir, out_shp=out_shp, connect8=True)
+    process_num = 12
+    raster2shapefile_large(in_tif, working_dir, out_shp=out_shp, connect8=True, process_num=process_num)
 
 
 def test_read_polygons_from_small_patch():
@@ -130,6 +162,27 @@ def test_read_polygons_from_small_patch():
     print(index)
 
 
+def test_build_adjacent_matrix_pygeo():
+    vector_path = os.path.expanduser('~/Data/dem_processing/grid_9053_tmp_files/20140701_dem_slope_bin_patches_all_polyTouchEdge.gpkg')
+    polygons = vector_gpd.read_polygons_gpd(vector_path,b_fix_invalid_polygon=False)
+    print('read %d polygons'%len(polygons))
+    # polygons = [item.buffer(1.0) for item in polygons]    # for complex polygons, buffer take a long time.
+    from pygeos import area, intersection,from_shapely,touches
+    import numpy as np
+    geo_polygons = from_shapely(polygons)
+    print(geo_polygons)
+
+    # inter_matrix = intersection(geo_polygons[:, np.newaxis], geo_polygons[np.newaxis, :])
+    # print(inter_matrix)
+
+    # print(touches(geo_polygons[:, np.newaxis], geo_polygons[np.newaxis, :]))
+    print(touches(geo_polygons, geo_polygons))
+
+    # area_matrix = area(intersection(geo_polygons[:, np.newaxis], geo_polygons[np.newaxis, :]))
+    # print(area_matrix)
+
+    # not working, there are some invalid polgyongs.
+    # buffer operation for polgyons take a lot of time, could be due to the complex polygons.
 
 
 def main(options, args):
@@ -137,14 +190,19 @@ def main(options, args):
     work_dir = options.work_dir
     in_raster = args[0]
     save_path = options.save_path
+    process_num = options.process_num
 
-    return raster2shapefile_large(in_raster, work_dir, out_shp=save_path, connect8=True)
+    # note: Sep 7, 2021. Not working. the last idea to try: need the row and coloumn for each patch,
+    # then merge polygons one patch by patch.
+
+    return raster2shapefile_large(in_raster, work_dir, out_shp=save_path, connect8=True,process_num=process_num)
 
 
 if __name__ == '__main__':
 
-    test_raster2shapefile_large()
+    # test_raster2shapefile_large()
     # test_read_polygons_from_small_patch()
+    test_build_adjacent_matrix_pygeo()
     sys.exit(0)
 
     usage = "usage: %prog [options] raster_path "
