3f818ec2c7c9c9e8919add60c9480c9d22e0225f
Lingcao Huang <yghlc@users.noreply.github.com>
2026-01-02 17:08:17 +0800

    sysnc to BigImageMapper

--- vector_gpd.py
+++ vector_gpd.py
@@ -1587,6 +1587,17 @@ def merge_vector_files(file_list, save_path,format='ESRI Shapefile'):
         command_string += ' "%s"'%item
     basic.os_system_exit_code(command_string)
 
+def merge_vector_files_geopandas(file_list, save_path,format='ESRI Shapefile'):
+    if os.path.isfile(save_path):
+        print(f'{save_path} exists, skip merge_vector_files_geopandas')
+        return
+    # Read each shapefile into a GeoDataFrame and store in a list
+    gdfs = [gpd.read_file(shp) for shp in file_list]
+    # Concatenate all GeoDataFrames
+    merged_gdf = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True))
+    # Save to a new shapefile
+    merged_gdf.to_file(save_path,driver=format)
+    basic.outputlogMessage(f'Saved to {save_path}')
 
 def merge_shape_files(file_list, save_path):
 
